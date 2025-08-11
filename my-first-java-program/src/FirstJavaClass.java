be7f951be80becd375d6d9a2bc6747877e75f101
NITYANSHI <nityanshisharma0103@gmail.com>
2025-08-12 01:37:02 +0530

    First java program made.

---
+++ my-first-java-program/src/FirstJavaClass.java
@@ -0,0 +1,31 @@
+public class FirstJavaClass {
+    public static void main(String[] args){
+
+       // dateOfBirth;// Variable declaration
+        // single line comment
+
+        /*        Multi-line comment
+        This is a multi-line comment.
+         */
+
+
+        System.out.println("Hello World");
+    }
+}
+
+
+
+
+//classABC{
+    //public static void main(String[] args){
+    //int x = 10;
+   // }
+//}
+
+// identifiers in java
+/*
+ a to z, A to Z,
+0 to 9, _ and $ are allowed.// Identifiers cannot start with a digit.
+// Keywords cannot be used as identifiers.
+ */
+
