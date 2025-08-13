11c959260503430b2d2a9a416edae91cefc02812
NITYANSHI <nityanshisharma0103@gmail.com>
2025-08-13 17:18:07 +0530

    increment operators discussed.

---
+++ src/FirstMainClass.java
@@ -0,0 +1,30 @@
+public class FirstMainClass {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = x++; // post-increment
+        int z = ++x; // pre-increment
+        System.out.println("Value of x : " + x);
+        System.out.println("Value of y : " + y);
+        System.out.println("Value of z : " + z);
+    }
+
+
+
+
+    // increment and decrement operators
+
+    // increment operators - pre-increment and post-increment
+    // decrement operators - pre-decrement and post-decrement
+
+    // pre-increment - value is incremented before it is used
+    // value is incremented first, then used in the expression
+
+    // post-increment - value is incremented after it is used
+    // value is used in the expression first, then incremented
+
+    // pre-decrement - value is decremented before it is used
+    // value is decremented first, then used in the expression
+
+    // post-decrement - value is decremented after it is used
+    // value is used in the expression first, then decremented
+}
